4007554455e7d83ae55302d32c43edc326c821be
Tim Schindler <tim.schindler@gmail.com>
2021-12-26 18:47:47 +0100

    correctly adds service based on App.config

--- ScenarioAlerter/Program.cs
+++ ScenarioAlerter/Program.cs
@@ -1,32 +1,49 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.UserSecrets;
 using ScenarioAlerter.AlertServices;
 using ScenarioAlerter;
 
-using IHost host = Host.CreateDefaultBuilder(args)
+
+IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureHostConfiguration(configHost =>
     {
         configHost.AddXmlFile("App.config");
     })
     .ConfigureServices((_, services) =>
+    {
         // "Options" pattern    
         services.AddSingleton(new AlerterOptions
         {
             LogFile = _.Configuration["logFile"],
-            AlertMethod = _.Configuration["alertMethod"]
         })
         .AddSingleton<IScenarioAlerter, Alerter>()
-        .AddSingleton(new PushoverConfig
-        {
-            UserToken = _.Configuration["pushoverConfig:userToken"],
-            ApplicationToken = _.Configuration["PUSHOVER_APPLICATIONTOKEN"]
-        })
-        .AddSingleton<IAlertService, PushoverService>()
         .AddHttpClient()
-        .AddLogging())
-        .Build();
+        .AddLogging();
+
+        var alertMethod = _.Configuration["alertMethod"];
+
+        if (alertMethod.Equals("Discord"))
+        {
+            services.AddSingleton(new DiscordConfig()
+            {
+                WebhookUri = _.Configuration["discordConfig:webhookUri"]
+            });
+            services.AddSingleton<IAlertService, DiscordService>();
+        }
+        else
+        {
+            services.AddSingleton(new PushoverConfig
+            {
+                UserToken = _.Configuration["pushoverConfig:userToken"],
+                // Environmental variable used at build time.
+                ApplicationToken = _.Configuration["PUSHOVER_APPLICATIONTOKEN"]
+            });
+            services.AddSingleton<IAlertService, PushoverService>();
+        }
+    })
+    .Build();
+
 
 host.Services.GetRequiredService<IScenarioAlerter>();
 
